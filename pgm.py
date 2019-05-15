0d338bb6248fa663c5991b4883fdb600b8c73109
Reshmi R <you@example.com>
2019-05-15 14:36:20 -0400

    third commit

--- pgm.py
+++ pgm.py
@@ -4,6 +4,10 @@ def add(num1, num2):
 def add2(num1,num2,num3):
 	sum=num1+num2+num3
 	return sum
+def add3(num1,num2,num3,num4):
+	sum=num1+num2+num3+num4
+	return sum
 
 print add(1,2)
 print add2(1,2,3)
+print add3(1,2,3,4)
